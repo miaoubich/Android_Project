c6f5c7317e30d8772f9e232ebfc2bb414742dd91
ali.bouzar@gmail.com <19810423A1l2i3>
2019-10-11 18:25:46 +0200

    finishing onRequestPermissionsResult method

--- app/src/main/java/com/example/crudtable/MainActivity.java
+++ app/src/main/java/com/example/crudtable/MainActivity.java
@@ -1,21 +1,81 @@
 package com.example.crudtable;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText mEdtName, mEdtAge, mEdtPhone;
     Button mBtnAdd, mBtnList;
     ImageView mImageView;
+    final int REQUEST_CODE_GALLERY = 999;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEdtName = findViewById(R.id.edtName);
+        mEdtAge = findViewById(R.id.edtAge);
+        mEdtPhone = findViewById(R.id.edtPhone);
+        mBtnAdd = findViewById(R.id.btnAdd);
+        mBtnList = findViewById(R.id.btnList);
+
+        //select image by imageview click
+        mImageView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //read external storage permission to select image from gallery
+                //runtime permission for devices android 6.0 and up
+                ActivityCompat.requestPermissions(
+                        MainActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        REQUEST_CODE_GALLERY
+                );
+            }
+        });
+
+        //add data to sqlite
+        mBtnAdd.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+            }
+        });
+
+        //display data list
+        mBtnList.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+            }
+        });
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if(requestCode == REQUEST_CODE_GALLERY){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                //gallery intent
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, REQUEST_CODE_GALLERY);
+            }else{
+                Toast.makeText(this, "You Don´t have permission to access this file", Toast.LENGTH_SHORT).show();
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode,permissions,grantResults);
+    }
+
 }
