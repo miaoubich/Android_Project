c59520ce5ff927757192eb7040dcd0126965a4c7
ali.bouzar@gmail.com <19810423A1l2i3>
2019-10-11 17:38:44 +0200

    Variables declaration

--- app/src/main/java/com/example/crudtable/MainActivity.java
+++ app/src/main/java/com/example/crudtable/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.crudtable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText mEdtName, mEdtAge, mEdtPhone;
+    Button mBtnAdd, mBtnList;
+    ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
